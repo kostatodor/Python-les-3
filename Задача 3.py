063adf12466cb12959feb5c3542084412c2be2c9
kostatodor <kosta88_88@mail.ru>
2022-10-10 13:56:58 +0300

    first commit

---
+++ Задача 3.py
@@ -0,0 +1,10 @@
+
+my_list = [1.1, 1.2, 2.1, 6, 10.03]
+min = 1
+max = 0
+for i in my_list:
+    if (i - int(i)) <= min:
+        min = i - int(i)
+    if (i - int(i)) >= max:
+        max = i - int(i)  
+print(max-min)
